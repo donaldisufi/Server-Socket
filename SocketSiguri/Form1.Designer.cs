7b514ffbd05ca48b61f6163574509160de4b1493
donaldisufi <blerinete@gmail.com>
2020-03-03 15:37:32 +0100

    inital commit

---
+++ SocketSiguri/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace SocketSiguri
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtMyPort = new System.Windows.Forms.TextBox();
+			this.txtMyIP = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.lsAllText = new System.Windows.Forms.ListBox();
+			this.txtMyMessage = new System.Windows.Forms.TextBox();
+			this.btnSend = new System.Windows.Forms.Button();
+			this.btnConnectedClients = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtMyPort
+			// 
+			this.txtMyPort.Location = new System.Drawing.Point(367, 34);
+			this.txtMyPort.Multiline = true;
+			this.txtMyPort.Name = "txtMyPort";
+			this.txtMyPort.Size = new System.Drawing.Size(138, 33);
+			this.txtMyPort.TabIndex = 5;
+			// 
+			// txtMyIP
+			// 
+			this.txtMyIP.Location = new System.Drawing.Point(106, 34);
+			this.txtMyIP.Multiline = true;
+			this.txtMyIP.Name = "txtMyIP";
+			this.txtMyIP.Size = new System.Drawing.Size(138, 33);
+			this.txtMyIP.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(293, 40);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(50, 20);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Port :";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(34, 40);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(34, 20);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "IP :";
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point(541, 32);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(109, 38);
+			this.btnStart.TabIndex = 2;
+			this.btnStart.Text = "Start server";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnConnect_Click);
+			// 
+			// lsAllText
+			// 
+			this.lsAllText.FormattingEnabled = true;
+			this.lsAllText.Location = new System.Drawing.Point(24, 104);
+			this.lsAllText.Name = "lsAllText";
+			this.lsAllText.Size = new System.Drawing.Size(626, 264);
+			this.lsAllText.TabIndex = 3;
+			// 
+			// txtMyMessage
+			// 
+			this.txtMyMessage.Location = new System.Drawing.Point(24, 423);
+			this.txtMyMessage.Multiline = true;
+			this.txtMyMessage.Name = "txtMyMessage";
+			this.txtMyMessage.Size = new System.Drawing.Size(523, 27);
+			this.txtMyMessage.TabIndex = 4;
+			// 
+			// btnSend
+			// 
+			this.btnSend.Location = new System.Drawing.Point(553, 423);
+			this.btnSend.Name = "btnSend";
+			this.btnSend.Size = new System.Drawing.Size(97, 27);
+			this.btnSend.TabIndex = 5;
+			this.btnSend.Text = "Send";
+			this.btnSend.UseVisualStyleBackColor = true;
+			this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+			// 
+			// btnConnectedClients
+			// 
+			this.btnConnectedClients.Location = new System.Drawing.Point(38, 483);
+			this.btnConnectedClients.Name = "btnConnectedClients";
+			this.btnConnectedClients.Size = new System.Drawing.Size(112, 49);
+			this.btnConnectedClients.TabIndex = 6;
+			this.btnConnectedClients.Text = "Show All Connected Clients";
+			this.btnConnectedClients.UseVisualStyleBackColor = true;
+			this.btnConnectedClients.Click += new System.EventHandler(this.btnConnectedClients_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(682, 555);
+			this.Controls.Add(this.btnConnectedClients);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtMyPort);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnSend);
+			this.Controls.Add(this.txtMyIP);
+			this.Controls.Add(this.txtMyMessage);
+			this.Controls.Add(this.lsAllText);
+			this.Controls.Add(this.btnStart);
+			this.Name = "Form1";
+			this.Text = " ";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox txtMyPort;
+		private System.Windows.Forms.TextBox txtMyIP;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.ListBox lsAllText;
+		private System.Windows.Forms.TextBox txtMyMessage;
+		private System.Windows.Forms.Button btnSend;
+		private System.Windows.Forms.Button btnConnectedClients;
+	}
+}
+
---
+++ SocketSiguri/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Configuration;
+using System.Security.Cryptography;
+
+namespace SocketSiguri
+{
+	public partial class Form1 : Form
+	{
+		public static double P = 13;
+		public static double G = 6;
+		private static double PrivateKey = 5;
+		public double PublicKey;
+		private double secretKey;
+		private double ClientPubKey = 9;
+
+		public static List<Socket> AllClients = new List<Socket>();
+
+		public bool AddNewClient(Socket obj)
+		{
+			foreach (var item in AllClients)
+			{
+				if(item == obj)
+				{
+					return false;
+				}
+			}
+			AllClients.Add(obj);	
+			return true;
+		}
+
+		public void ShowAllConnectedClients()
+		{
+			string IPs = "";
+			foreach (var item in AllClients)
+			{
+				IPs += ((IPEndPoint)(item.RemoteEndPoint)).Address.ToString()+ " \n";
+			}
+			MessageBox.Show(IPs);
+		}
+
+		Socket GetSocket;
+		Socket listenerSocket;
+		TcpListener	tcplistener;
+		IPEndPoint ip;
+		IPAddress ipAdresa;
+		Socket Client;
+		bool start = false;
+
+		byte[] buffer;
+
+	
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//   sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			//sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+			//txtMyIP.Text = GetLocalIP();
+		
+		}
+		public void ConnectAndShareKey()
+		{
+
+			//MessageBox.Show(PublicKey.ToString());
+			//2
+		}
+		public void GetPubKeySetSecreteKey()
+		{
+			secretKey = Math.Pow(ClientPubKey, PrivateKey) % P;
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+			btnStart.Text = "Starto serverin";
+			PublicKey = Math.Pow(G, PrivateKey) % P;
+			GetPubKeySetSecreteKey();
+		}
+
+		private bool TryConvertIP(string ip)
+		{
+			try
+			{
+				IPAddress.Parse(ip);
+				return true;
+			}
+			catch (Exception)
+			{
+			  return false;
+			}
+		}
+		private bool isNumber(string port)
+		{
+			try
+			{
+				Int32.Parse(port);
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		private void btnConnect_Click(object sender, EventArgs e)
+		{
+
+
+			if (btnStart.Text == "Starto serverin")
+			{
+
+				if (TryConvertIP(txtMyIP.Text) && isNumber(txtMyPort.Text))
+				{
+
+					btnStart.Text = "Ndalo serverin";
+					start = true;
+					Application.DoEvents();
+					IPAddress ip = IPAddress.Parse(txtMyIP.Text);
+					StartServer(ip,int.Parse(txtMyPort.Text));
+
+				}else
+				{
+					MessageBox.Show("Port or IP wasn't in correct format!");
+				}
+			}
+			else
+			{
+				btnStart.Text = "Starto serverin";
+				tcplistener.Stop();
+				lsAllText.Items.Add("U Ndal Serveri !!! ");
+
+				start = false;
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+		private async void  StartServer(IPAddress ip , int porti)
+		{
+			    lsAllText.Items.Add("Serveri u leshua ");
+				GetPubKeySetSecreteKey();
+				tcplistener = new TcpListener(ip, porti);
+				tcplistener.Start();
+				while (start)
+				{
+					try
+					{
+						listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+						listenerSocket = await tcplistener.AcceptSocketAsync();
+					    AddNewClient(listenerSocket);
+				        ipAdresa = ((IPEndPoint)(listenerSocket.RemoteEndPoint)).Address;
+		     	    	Application.DoEvents();
+
+					     buffer = new byte[2048];
+					     byte[] sendBuffer = new byte[2048];
+			   
+					     int byteNumbers = listenerSocket.Receive(buffer);
+					     
+					MessageBox.Show(secretKey.ToString() + "Server Secret");
+					
+					if (byteNumbers > 0)
+					{
+						//// pjesa per qelsin 
+						//byte[] bufferPranues = new byte[1024];
+						//byte[] madhMes = new byte[4];
+						//byte[] mesazhiPr = new byte[byteNumbers - 4];
+						//Buffer.BlockCopy(bufferPranues, 0, madhMes, 0, 4);
+						//int msgSize = BitConverter.ToInt32(madhMes, 0);
+						//Buffer.BlockCopy(bufferPranues, 4, mesazhiPr, 0, msgSize);
+						//string msgText = ASCIIEncoding.ASCII.GetString(mesazhiPr);
+
+						//if (ASCIIEncoding.ASCII.GetString(madhMes) == (100).ToString())
+						//{
+						//	//txtMesazhetServeri.AppendText("Celsi publik i serverit : " + msgText + "\n");
+						//	GetPubKeySetSecreteKey(double.Parse(ASCIIEncoding.ASCII.GetString(mesazhiPr)));
+						//	Application.DoEvents();
+
+						//	byte[] bufferDergues;
+						//	byte[] buffer;
+
+						//	string mesazhi = PublicKey.ToString();
+						//	buffer = ASCIIEncoding.ASCII.GetBytes(mesazhi);
+
+						//	//byte[] buffer = new byte[mesazhi.Length + 4];
+						//	int numDetect = 100;
+						//	byte[] firstNumbers = new byte[4];
+						//	firstNumbers = ASCIIEncoding.ASCII.GetBytes(numDetect.ToString());
+						//	int totalGjatsia = firstNumbers.Length + buffer.Length;
+						//	bufferDergues = new byte[totalGjatsia];
+						//	Buffer.BlockCopy(firstNumbers, 0, bufferDergues, 0, 4);
+						//	Buffer.BlockCopy(buffer, 0, bufferDergues, 4, bufferDergues.Length);
+
+						//	listenerSocket.Send(bufferDergues);
+						//}
+						//else
+						//{
+
+
+
+
+						/// pjesa per jo qelsin 
+						   string message = ASCIIEncoding.UTF8.GetString(buffer, 0, byteNumbers);
+						  // MessageBox.Show(message);
+					     	//string message = Convert.ToBase64String(buffer,0,byteNumbers);
+					      //  lsAllText.Items.Add("Klienti : " + DESDecrypt(message));
+						lsAllText.Items.Add("Klienti : " + message);
+
+						//string dergoMesazh = DESEncrypt("Mesazhi u pranua nga serveri");
+						string dergoMesazh = "Mesazhi u pranua nga serveri";
+
+						//enkripton mesazhin
+						byte[] MadhesiaMesazhit = new byte[4];
+
+							int mesINT = dergoMesazh.Length;
+							MadhesiaMesazhit = BitConverter.GetBytes(mesINT);
+							byte[] bufferidergues = ASCIIEncoding.ASCII.GetBytes(dergoMesazh);
+							int madhesiaTotale = 4 + bufferidergues.Length;
+							byte[] mesazhiFinal = new byte[madhesiaTotale];
+							Buffer.BlockCopy(MadhesiaMesazhit, 0, mesazhiFinal, 0, 4);
+							Buffer.BlockCopy(bufferidergues, 0, mesazhiFinal, 4, bufferidergues.Length);
+
+							Client = listenerSocket;
+							listenerSocket.Send(mesazhiFinal);
+
+
+						//}
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+			}
+		
+		}
+
+		private static string GetLocalIP()
+		{
+			//IPHostEntry host;
+			//host = Dns.GetHostEntry(Dns.GetHostName());
+
+			//foreach(IPAddress ip in host.AddressList)
+			//{
+			//	if(ip.AddressFamily == AddressFamily.InterNetwork)
+			//	{
+			//		return ip.ToString();
+			//	}
+			//}
+			//return "127.0.0.1";
+			return "";
+		}
+
+		private void btnSend_Click(object sender, EventArgs e)
+		{
+			if(txtMyMessage.Text.Length >= 1) {
+
+				//enkripton mesazhi
+				try
+				{
+					byte[] MadhesiaMesazhit = new byte[4];
+					int mesINT = txtMyMessage.Text.Length;
+					MadhesiaMesazhit = BitConverter.GetBytes(mesINT);
+					byte[] bufferidergues = ASCIIEncoding.ASCII.GetBytes(txtMyMessage.Text);
+					int madhesiaTotale = 4 + bufferidergues.Length;
+					byte[] mesazhiFinal = new byte[madhesiaTotale];
+					Buffer.BlockCopy(MadhesiaMesazhit, 0, mesazhiFinal, 0, 4);
+					Buffer.BlockCopy(bufferidergues, 0, mesazhiFinal, 4, bufferidergues.Length);
+					Client.Send(mesazhiFinal);
+					lsAllText.Items.Add("Serveri : " + txtMyMessage.Text);
+				}
+				catch (Exception)
+				{
+					MessageBox.Show("Coudln't send message to the Client");
+				}
+			}
+				
+
+
+		}
+
+		private void MessageCallBack(IAsyncResult aResult )
+		{
+			//try
+			//{
+			//	byte[] recievedData = new byte[2000];
+			//	recievedData = (byte[])aResult.AsyncState;
+
+			//	ASCIIEncoding encoding = new ASCIIEncoding();
+			//	string recievedMessage = encoding.GetString(recievedData);
+
+			//	lsAllText.Items.Add("Other PC : "+ recievedData);
+			//	buffer = new byte[2000];
+			//	sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None,ref ipRemote, new AsyncCallback(MessageCallBack), buffer);
+			//}
+			//catch (Exception ex)
+			//{
+			//	MessageBox.Show(ex.Message);
+			//}
+	
+		}
+
+		private void btnConnectedClients_Click(object sender, EventArgs e)
+		{
+			ShowAllConnectedClients();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
